b21a029cd0212b9c17b5dd31cea9bfe5a0327c8f
Kurkuruzo <kurkuruzo@gmail.com>
2024-06-18 18:05:26 +0300

    Fixes for docker and cleanup

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 from dispatcher import start_polling
 
-logging.basicConfig(level=logging.INFO, filename="tg_informer.log")
+logging.basicConfig(level=logging.INFO, filename="mechanics_bot.log")
 
 if __name__ == "__main__":
     asyncio.run(start_polling())
